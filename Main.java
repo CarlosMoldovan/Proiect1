eba9738487ad36e1cfbabb8d48fbf293bc15c43c
CarlosMoldovan <141249237+CarlosMoldovan@users.noreply.github.com>
2023-10-10 11:01:19 +0300

    Commit2

--- Main.java
+++ Main.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
-// Clasa 'Main' conține metoda 'main' care este punctul de pornire al programului.
+// Clasa 'Main' contine metoda 'main' care este punctul de pornire al programului.
 public class Main {
     public static void main(String[] args) {
-        // Crearea unui obiect 'systemstelar1' de tip 'systemsolar' pentru gestionarea informațiilor despre sistemul solar.
+        // Crearea unui obiect 'systemstelar1' de tip 'systemsolar' pentru gestionarea informatiilor despre sistemul solar.
         systemsolar systemstelar1 = new systemsolar();
         int meniu = 100;
         Scanner sc = new Scanner(System.in);
-        // Un meniu interactiv care permite utilizatorului să introducă și să afișeze informații despre sistemul solar.
+        // Un meniu interactiv care permite utilizatorului sa introduca si sa afiseze informatii despre sistemul solar.
         while (meniu != 0) {
-            // Afișarea opțiunilor disponibile în meniu.
+            // Afisarea optiunilor disponibile in meniu.
             System.out.println("0.Iesire program!");
             System.out.println("1.Introdu numele sistemului stelar!");
             System.out.println("2.Introdu nr stele!");
             System.out.println("3.Introdu nr planete!");
-            System.out.println("4.Introdu dacă sunt forme de viață în acel sistem stelar(1-yes/0-not)!");
+            System.out.println("4.Introdu daca sunt forme de viata in acel sistem stelar(1-yes/0-not)!");
             System.out.println("5.Numele sistemului!");
             System.out.println("6.Nr stele!");
             System.out.println("7.Nr planete!");
-            System.out.println("8.Există sau nu viață în acel sistem stelar");
+            System.out.println("8.Exista sau nu viata in acel sistem stelar");
             System.out.println("9.Metoda toString!");
-            System.out.println("Introdu o variantă:");
+            System.out.println("Introdu o varianta:");
             
-            // Citirea opțiunii selectate de utilizator.
+            // Citirea optiunii selectate de utilizator.
             meniu = sc.nextInt();
             sc.nextLine();
 
-            // Un switch pentru a gestiona diferitele opțiuni din meniu.
+            // Un switch pentru a gestiona diferitele optiuni din meniu.
             switch (meniu) {
                 case 0:
                     break;
                 case 1:
-                    // Utilizatorul introduce numele sistemului stelar și îl setăm în obiectul 'systemstelar1'.
+                    // Utilizatorul introduce numele sistemului stelar si il setam in obiectul 'systemstelar1'.
                     String nume;
                     System.out.println("Introdu numele sistemului stelar:");
                     nume = sc.nextLine();
                     systemstelar1.setNumeSistem(nume);
                     break;
                 case 2:
-                    // Utilizatorul introduce numărul de stele și îl setăm în obiectul 'systemstelar1'.
+                    // Utilizatorul introduce numarul de stele si il setam in obiectul 'systemstelar1'.
                     int nr;
                     System.out.println("Introdu nr stele:");
                     nr = sc.nextInt();
@@ -45,7 +45,7 @@ public class Main {
                     systemstelar1.setNrStele(nr);
                     break;
                 case 3:
-                    // Utilizatorul introduce numărul de planete și îl setăm în obiectul 'systemstelar1'.
+                    // Utilizatorul introduce numarul de planete si il setam in obiectul 'systemstelar1'.
                     int nrp;
                     System.out.println("Introdu nr planete:");
                     nrp = sc.nextInt();
@@ -53,7 +53,7 @@ public class Main {
                     systemstelar1.setNrPlanete(nrp);
                     break;
                 case 4:
-                    // Utilizatorul introduce dacă există forme de viață și setăm valoarea în obiectul 'systemstelar1'.
+                    // Utilizatorul introduce daca exista forme de viata si setam valoarea in obiectul 'systemstelar1'.
                     boolean x;
                     System.out.println("Introdu 1 sau 0:");
                     x = sc.nextBoolean();
@@ -61,32 +61,32 @@ public class Main {
                     systemstelar1.setExistaViata(x);
                     break;
                 case 5:
-                    // Afișăm numele sistemului solar utilizând metoda 'getNumeSistem'.
+                    // Afisam numele sistemului solar utilizând metoda 'getNumeSistem'.
                     System.out.println("Numele sistemului este " + systemstelar1.getNumeSistem());
                     break;
                 case 6:
-                    // Afișăm numărul de stele utilizând metoda 'getNrStele'.
+                    // Afisam numarul de stele utilizand metoda 'getNrStele'.
                     System.out.println("Sistemului are " + systemstelar1.getNrStele() + " stele");
                     break;
                 case 7:
-                    // Afișăm numărul de planete utilizând metoda 'getNrPlanete'.
+                    // Afisam numarul de planete utilizand metoda 'getNrPlanete'.
                     System.out.println("Sistemului are " + systemstelar1.getNrPlanete() + " planete");
                     break;
                 case 8:
-                    // Afișăm dacă există sau nu viață în sistem utilizând metoda 'existaFormeDeViata'.
+                    // Afisam daca exista sau nu viata in sistem utilizand metoda 'existaFormeDeViata'.
                     if (systemstelar1.existaFormeDeViata()) {
-                        System.out.println("Există viață în acest sistem stelar!");
+                        System.out.println("Exista viata in acest sistem stelar!");
                     } else {
-                        System.out.println("Nu există viață în acest sistem stelar!");
+                        System.out.println("Nu exista viata in acest sistem stelar!");
                     }
                     break;
                 case 9:
-                    // Afișăm informații despre sistem folosind metoda 'toString'.
+                    // Afisam informatii despre sistem folosind metoda 'toString'.
                     System.out.println(systemstelar1);
                     break;
                 default:
-                    // Afișăm un mesaj de eroare pentru opțiunea incorectă din meniu.
-                    System.out.println("Opțiune greșită");
+                    // Afisam un mesaj de eroare pentru optiunea incorecta din meniu.
+                    System.out.println("Optiune gresita");
                     break;
             }
         }
--- systemsolar.java
+++ systemsolar.java
@@ -1,13 +1,13 @@
-// Clasa 'systemsolar' reprezintă un sistem solar și conține atribute și metode pentru gestionarea informațiilor despre acest sistem.
+// Clasa 'systemsolar' reprezinta un sistem solar si contine atribute si metode pentru gestionarea informatiilor despre acest sistem.
 
 public class systemsolar{
-        // Variabile de instanță pentru a stoca informații despre sistemul solar
+        // Variabile de instanta pentru a stoca informatii despre sistemul solar
         String nume;
         int nrStele;
         private int nrPlanete;
         private boolean existaFormeDeViata;
     
-        // Constructorul clasei care inițializează variabilele cu valori implicite.
+        // Constructorul clasei care initializeaza variabilele cu valori implicite.
         public systemsolar() {
             nume = null;
             nrStele = 0;
@@ -19,44 +19,44 @@ public class systemsolar{
             this.nume = nume;
         }
     
-        // Metoda pentru a seta numărul de stele din sistem.
+        // Metoda pentru a seta numarul de stele din sistem.
         public void setNrStele(int nrStele) {
             this.nrStele = nrStele;
         }
     
-        // Metoda pentru a seta numărul de planete din sistem.
+        // Metoda pentru a seta numarul de planete din sistem.
         public void setNrPlanete(int nrPlanete) {
             this.nrPlanete = nrPlanete;
         }
     
-        // Metoda pentru a seta existența formelor de viață în sistem.
+        // Metoda pentru a seta existenta formelor de viata in sistem.
         public void setExistaViata(boolean existaFormeDeViata) {
             this.existaFormeDeViata = existaFormeDeViata;
         }
     
-        // Metodă pentru a obține numele sistemului solar.
+        // Metoda pentru a obtine numele sistemului solar.
         public String getNumeSistem() {
             return nume;
         }
     
-        // Metodă pentru a obține numărul de stele din sistem.
+        // Metoda pentru a obtine numarul de stele din sistem.
         public int getNrStele() {
             return nrStele;
         }
     
-        // Metodă pentru a obține numărul de planete din sistem.
+        // Metoda pentru a obtine numarul de planete din sistem.
         public int getNrPlanete() {
             return nrPlanete;
         }
     
-        // Metodă pentru a verifica dacă există forme de viață în sistem.
+        // Metoda pentru a verifica daca exista forme de viata in sistem.
         public boolean existaFormeDeViata() {
             return existaFormeDeViata;
         }
     
-        // Suprascrierea metodei toString() pentru a afișa informații despre sistem sub formă de șir de caractere.
+        // Suprascrierea metodei toString() pentru a afișa informatii despre sistem sub forma de sir de caractere.
         @Override
         public String toString() {
-            return "Sistemul stelar se numește " + nume + ", are " + nrStele + " stele, și " + nrPlanete + " planete!";
+            return "Sistemul stelar se numeste " + nume + ", are " + nrStele + " stele, si " + nrPlanete + " planete!";
         }
 }
